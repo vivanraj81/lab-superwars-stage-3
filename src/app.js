3c1898e7f09b8762546b0d6cbd22a96c011b776d
vivan raj <vivanrajmittakodi6@gmail.com>
2023-11-05 20:39:20 +0530

    update

--- src/app.js
+++ src/app.js
@@ -24,9 +24,19 @@ const PLAYERS = [
 // initialize players with image and strength
 const initPlayers = (players) => {
     let detailedPlayers = '';
-
+    var i=0
     // Instead of forloop use Map method
     // Code here
+    detailedPlayers = players.map(function(item,index){
+        if (index%2==0){
+            type1="hero"
+        }else{
+            type1="villain"
+        }
+        i++
+        return item={name:players[i-1],strength:getRandomStrength(),image:`./images/super-${i}.png`,type:type1}
+       
+    })
 
     return detailedPlayers;
 }
@@ -43,6 +53,22 @@ const buildPlayers = (players, type) => {
     // Instead of using for loop
     // Use chaining of Array methods - filter, map and join
     // Type your code here
+    var hero = players.filter(function(item){
+        return item.type=="hero"
+    })
+    var villain = players.filter(function(item){
+        return item.type=="villain"
+    })
+    if (type=="hero"){
+        fragment=hero.map(function(item){
+            return `<div class="player"><img src="${item.image}"alt=""><div class="name">${item.name}</div><div class="strength">${item.strength}</div></div>`
+        }).join("")
+    }else{
+        fragment=villain.map(function(item){
+            return `<div class="player"><img src="${item.image}"alt=""><div class="name">${item.name}</div><div class="strength">${item.strength}</div></div>`
+        }).join("")
+    }
+    console.log(fragment)
 
     return fragment;
 }
